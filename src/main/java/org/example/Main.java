d715a556a1761c9af1355cdf528735264da5ad88
PerminovPavel <98221254+PerminovPavel@users.noreply.github.com>
2023-03-05 23:28:38 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+    String API = "5e511fab-209a-4cf2-9cda-5db545768579";
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Введите название организации режим работы которой вы хотите узнать:");
+    String nameOfOrg=sc.nextLine().replace(" ","+");
+
+    HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://search-maps.yandex.ru/v1/?apikey="+API+"&text="+nameOfOrg+"&lang=ru_RU&type=biz")).GET().build();
+    HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    JSONObject jo = new JSONObject(response.body());
+    JSONArray res = jo.getJSONArray("features");
+
+    if (res.length() == 0) {
+      System.out.println("404 not found");
+    } else {
+
+    System.out.println("Всего нашлось "+res.length()+" организаций"); System.out.println("");
+    for (Object org : res){
+      String timeOfWork = null;
+
+      JSONObject out = ((JSONObject)org).getJSONObject("properties").getJSONObject("CompanyMetaData");
+      String nameOfOrgOut = out.getString("name");
+      String address = out.getString("address");
+      try {
+        timeOfWork = out.getJSONObject("Hours").getString("text");
+      } catch (JSONException e) {
+        timeOfWork = "не известен";
+      }
+      System.out.println("Название организации: " + nameOfOrgOut);
+      System.out.println("Адрес организации: " + address);
+      System.out.println("Режим работы: " + timeOfWork);
+      System.out.println("");
+    }
+    }
+  }
+}
